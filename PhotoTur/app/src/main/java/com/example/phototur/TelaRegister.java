d42a33469a0bf08bf8ac9ca96eb4b210f72ed409
Jéssica Carvalho <63794913+jessiwellen@users.noreply.github.com>
2020-08-29 21:24:48 -0300

    Add files via upload

---
+++ PhotoTur/app/src/main/java/com/example/phototur/TelaRegister.java
@@ -0,0 +1,14 @@
+package com.example.phototur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_register);
+    }
+}
